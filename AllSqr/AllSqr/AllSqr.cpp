34d344512951935d256452c89be7c17620c17ceb
LiubovPil <alexandraelle95@gmail.com>
2022-07-28 23:30:02 +0300

    AllSqr add

---
+++ AllSqr/AllSqr/AllSqr.cpp
@@ -0,0 +1,11 @@
+﻿// Tasks from the course "Fundamentals of C++: Yellow Belt
+// Done by Liubov Piliagina
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
