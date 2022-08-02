19ce76f5be9ee85812189a8559d2334b45c9928a
LiubovPil <alexandraelle95@gmail.com>
2022-08-02 17:23:44 +0300

    Rectangle finish

---
+++ Rectangle_programm/Rectangle_programm/Rectangle_programm.cpp
@@ -0,0 +1,11 @@
+﻿// Tasks from the course "Fundamentals of C++: Yellow Belt
+// Done by Liubov Piliagina
+
+#include <iostream>
+
+#include "Rectangle.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
