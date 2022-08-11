38db857d524d26c5df25b9ef9c74be5acac37682
LiubovPil <alexandraelle95@gmail.com>
2022-08-11 14:02:10 +0300

    Add and finish DoItYourselfInheritance

---
+++ DoItYourselfInheritance/DoItYourselfInheritance/DoItYourselfInheritance.cpp
@@ -0,0 +1,35 @@
+﻿// Tasks from the course "Fundamentals of C++: Yellow Belt
+// Done by Liubov Piliagina
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Animal {
+public:
+    Animal(const string& name)
+        : Name(name) {
+    }
+
+    const string Name;
+};
+
+
+class Dog : public Animal{
+public:
+    Dog(const string& name) : Animal(name) {
+    }
+
+    void Bark() {
+        cout << Name << " barks: woof!" << endl;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
